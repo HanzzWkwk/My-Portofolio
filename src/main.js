db2177d4a5048409d1c764753b786e0f7e67fb55
HanzzWkwk <ahmadfarhan2000a@email.com>
2025-11-29 08:20:36 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+document.getElementById("sub").addEventListener("click", () => {
+  setTimeout(() => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+  }, 5);
+});
